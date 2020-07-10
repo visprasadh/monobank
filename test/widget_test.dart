3aa7e92ed229cbe9d86313c7d3f176d6db813146
Vishnuprasadh <visprasadhkumaravelu@gmail.com>
2020-07-10 11:54:30 +0530

    Prelims

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:monobank/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MonoBank());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
