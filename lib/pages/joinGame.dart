ac17177bcea7b386731e9831f71e3c094f7d4668
visprasadh <visprasadhkumaravelu@gmail.com>
2020-08-11 23:23:41 +0530

    Updated Join Game page

--- lib/pages/joinGame.dart
+++ lib/pages/joinGame.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:monobank/widgets/header.dart';
+import 'package:monobank/widgets/blackButton.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
 class JoinGame extends StatefulWidget {
   @override
@@ -6,12 +9,43 @@ class JoinGame extends StatefulWidget {
 }
 
 class _JoinGameState extends State<JoinGame> {
+  String qr;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[],
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Header(),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Text(
+                      'Gametag: $qr',
+                      style: TextStyle(
+                        fontFamily: 'Coves',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            BlackButton(label: 'Scan code', onPressed: scan),
+          ],
+        ),
       ),
     );
+  }
+
+  Future scan() async {
+    ScanResult code = await BarcodeScanner.scan();
+    String qr = code.rawContent;
+    setState(() => this.qr = qr);
   }
 }
